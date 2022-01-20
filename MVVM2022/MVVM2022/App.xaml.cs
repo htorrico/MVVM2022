7d0772380ba408ebba396ce39ab802582f1b1219
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2022-01-19 20:55:24 -0500

    Agregar archivos de proyecto.

---
+++ MVVM2022/MVVM2022/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MVVM2022.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM2022
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Operations();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MVVM2022/MVVM2022/ViewModels/OperationsViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM2022.ViewModels
+{
+    public class OperationsViewModel : ViewModelBase
+    {
+        
+
+        int valor1;
+        public int Valor1
+        {
+            get { return valor1; }
+            set
+            {
+                if (valor1 != value)
+                {
+                    valor1 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        int valor2;
+        public int Valor2
+        {
+            get { return valor2; }
+            set
+            {
+                if (valor2 != value)
+                {
+                    valor2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        int resultadoSuma;
+        public int ResultadoSuma
+        {
+            get { return resultadoSuma; }
+            set
+            {
+                if (resultadoSuma != value)
+                {
+                    resultadoSuma = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        int resultadoResta;
+        public int ResultadoResta
+        {
+            get { return resultadoResta; }
+            set
+            {
+                if (resultadoResta != value)
+                {
+                    resultadoResta = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public ICommand Sumar { protected set; get; }
+        public ICommand Restar { protected set; get; }
+
+
+        public OperationsViewModel()
+        {
+            Sumar = new Command(() =>
+            {                
+                ResultadoSuma = Valor1 + Valor2;
+            });
+
+            Restar = new Command(() =>
+            {
+                ResultadoResta = Valor1 - Valor2;
+            });
+
+        }
+
+
+    }
+}
